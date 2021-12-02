79f4d29700d9141b1593c83f1ae1e84450f167a1
Thomas Mathews <thomas@monstercat.com>
2021-12-02 12:57:12 -0800

    Add cache controlling.
    This allows you to force the generation of cached images with a query
    param. Additionally it now stores the cache information from the image
    request and stores that on its end. This allows us to check if the image
    we are serving has expired and to resize again.

--- file-system.go
+++ file-system.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"time"
 )
+
+var ErrNoFile = errors.New("no file")
 
 type FileSystem interface {
 	FromVolume(string) FileSystem
 	ObjectURL(string) string
-	Exists(string) (bool, error)
+	Info(string) (FileInfo, error)
 	ReadCloser(string) (io.ReadCloser, error)
-	Write(string, io.Reader) error
+	Write(string, io.Reader, FileInfoWrite) error
 	Delete(string) error
+}
+
+type FileInfoRead interface {
+	Created() time.Time
+}
+
+type FileInfoWrite interface {
+	CacheControl() string
+}
+
+type FileInfo interface {
+	FileInfoWrite
+	FileInfoRead
 }
